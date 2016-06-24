fcccd95618c4f9eab645fea48de8743f57cc4e48
Wei Kiat Gerald Ang <GeraldAngW@Weis-MacBook-Pro.local>
2016-06-24 22:18:57 +0800

    added Create Board functionality via DOM

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardThree === cardFour) {
+/*if (cardThree === cardFour) {
 alert("You found a match!")
 }
 else {
 	alert("Sorry, try again.")
+}*/
+
+function createBoard () {
+	var gameboard = document.createElement('div')
+	gameboard.id = 'gameboard'
+	document.getElementById('Game').appendChild(gameboard)
+	for (i=0;i<4;i+=1) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		document.getElementById('gameboard').appendChild(card);
+	}
 }
+
+
+
+
+
+
+
